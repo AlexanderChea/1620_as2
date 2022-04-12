f45d07ce7ea30ef76d1dc57ef5f7cd892d8d0936
Alex <alexanderchea6637@hotmail.com>
2022-04-11 20:17:00 -0700

    add note taking area to app.js

--- src/app.js
+++ src/app.js
@@ -5,3 +5,21 @@ const notes = [
     id: 1 
   }
 ]
+
+// Creating Note taking area
+
+let addButton = document.querySelector('i')
+
+let writingArea = document.querySelector('.write-note-area')
+
+let textarea = "<textarea id='note-textarea'></textarea> <button id='save'>Save</button> <button id='cancel_note'>Cancel</button>"
+
+function addnote(evt) {
+  writingArea.insertAdjacentHTML('afterbegin', textarea)
+  let cancel_button = document.querySelector('#cancel_note')
+  let save =  document.querySelector('#save')
+  cancel_button.addEventListener('click', remove_Note)
+  save.addEventListener('click', saveNote)
+}
+
+addButton.addEventListener('click', addnote)
